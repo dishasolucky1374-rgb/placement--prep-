7972d01b41d32eb2c349d0e2cdfc295131f95c6b
dishasolucky1374-rgb <dishasolucky1374@gmail.com>
2026-04-01 08:14:59 +0530

    Create day 4 reverse string.c

---
+++ day 4 reverse string.c
@@ -0,0 +1,14 @@
+#include<stdio.h>
+#include<string.h>
+void reverseString(char* s, int sSize) {
+    int i=0;
+    int j=sSize-1;
+    char temp;
+    while(i<j){
+    temp=s[i];
+    s[i]=s[j];
+    s[j]=temp;
+    i++;
+    j--;
+    }
+} 
